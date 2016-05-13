0545b6cb330bc7210d27f38358bce93f29e9a088
Mike Ulvila <mikeulvila@me.com>
2016-05-13 12:38:24 -0500

    refactored to component

--- app/component.js
+++ app/component.js
@@ -5,4 +5,39 @@ angular
   })
   .component('counter', {
 
+    bindings: {
+      count: '='
+    },
+    controller: function () {
+      function increment() {
+        this.count++;
+      }
+      function decrement() {
+        this.count--;
+      }
+      this.increment = increment;
+      this.decrement = decrement;
+    },
+    template: [
+      '<div class="todo">',
+        '<input type="text" ng-model="$ctrl.count">',
+        '<button type="button" ng-click="$ctrl.decrement();">-</button>',
+        '<button type="button" ng-click="$ctrl.increment();">+</button>',
+      '</div>'
+    ].join('')
+
+    // The template property can be defined as a function that is now injected with
+    // $element and $attrs locals. If the template property is a function then it
+    // needs to return an String representing the HTML to compile:
+
+      // template: function ($element, $attrs) {
+      //   // access to $element and $attrs
+      //   return [
+      //     '<div class="todo">',
+      //       '<input type="text" ng-model="$ctrl.count">',
+      //       '<button type="button" ng-click="$ctrl.decrement();">-</button>',
+      //       '<button type="button" ng-click="$ctrl.increment();">+</button>',
+      //     '</div>'
+      //   ].join('')
+      // }
   });
